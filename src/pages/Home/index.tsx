edb9fe955a39d02c6e8ab4b3a611787e39280a28
ManuCoutinho <gdn.sasha@gmail.com>
2023-07-16 13:32:40 -0300

    feat: :sparkle: create main page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,30 @@
+import { ChatCircle } from "@phosphor-icons/react"
+import { useAppSelector } from "@/store"
+import { Header } from "@/components/Header"
+import { Module } from "@/components/Module"
+import { Video } from "@/components/Video"
+
 export const Home: React.FC = () => {
+  const modules = useAppSelector(state => state.player.course.modules)
+  
   return (
-    <main className='bg-night w-screen h-screen'>
-   
-    </main>
+    <div className='bg-night w-screen h-screen text-info flex justify-center items-center'>
+      <div className='flex w-[1100px] flex-col gap-6'>
+        <div className='flex items-center justify-between'>
+          <Header />
+          <button className='flex items-center gap-2 rounded bg-secondary px-3 text-sm font-medium text-info hover:bg-amber-600 transition-colors ease-in-out delay-200'>
+            <ChatCircle size={32} weight="light" />
+          </button>
+        </div>
+        <main className="relative flex overflow-hidden rounded-lg border border-forest bg-neutral-800 shadow pr-80">
+          <div className="flex-1">
+            <Video />
+          </div>
+          <aside className="w-80 absolute inset-0 border-l divide-y-2 divide zinc-800 bg-zinc-900 overflow-y-auto">
+            <Module />
+          </aside>
+        </main>
+      </div>
+    </div>
   )
 }
